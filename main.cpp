24cd36262696f975067797ed8d28b81781a9739f
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2016-09-17 23:59:25 +0300

    Testing main body for power of 2

--- main.cpp
+++ main.cpp
@@ -9,19 +9,25 @@
 #include <cassert>
 int main()
 {
-    const unsigned c_uParam = 16;
-    const unsigned c_uTestVal = 40;
+    const unsigned c_uParam = 15;
+    const double c_uPow = std::log2(c_uParam);
 
-    CEncoder encoder(c_uParam);
-    encoder.putbits(c_uTestVal);
-    const unsigneds_vec& c_vBuf = encoder.getBuf();
+    CGeomDistr distr(0.01f);
+    for(unsigned i = 0; i < 1000; i++)
+    {
+        const unsigned c_uTestVal = distr.get();
 
-    std::copy(c_vBuf.begin(), c_vBuf.end(),
-                std::ostream_iterator<unsigned>(std::cout, " "));
-    std::cout << std::endl;
+        CEncoder encoder(c_uParam);
+        encoder.putbits(c_uTestVal);
+        const unsigneds_vec& c_vBuf = encoder.getBuf();
 
-    CDecoder decoder(c_uParam);
-    const unsigned c_uDecoded = decoder.getBits(c_vBuf);
-    assert(c_uDecoded == c_uTestVal);
+        std::copy(c_vBuf.begin(), c_vBuf.end(),
+                  std::ostream_iterator<unsigned>(std::cout, " "));
+        std::cout << std::endl;
+
+        CDecoder decoder(c_uParam);
+        const unsigned c_uDecoded = decoder.getBits(c_vBuf);
+        assert(c_uDecoded == c_uTestVal);
+    }
     return 0;
 }
