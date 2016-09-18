edec359a6788b43e395d15207763e9ee5c2aaafa
Toniy Tsvetaev <vespertilioamentis@yandex.ru>
2016-09-18 18:57:24 +0300

    Looped experiments

--- main.cxx
+++ main.cxx
@@ -9,23 +9,22 @@
 #include <cassert>
 int main()
 {
-    const unsigned c_uParam = 4;
+    const unsigned c_uParam = 15;
 
     CGeomDistr distr(0.01f);
 
-    const unsigned c_uTestVal = 9;
+    for(unsigned i = 0; i < 10000; i++)
+    {
+        const unsigned c_uTestVal = distr.get();
 
-    CEncoder encoder(c_uParam);
-    encoder.putbits(c_uTestVal);
-    const unsigneds_vec& c_vBuf = encoder.getBuf();
+        CEncoder encoder(c_uParam);
+        encoder.putbits(c_uTestVal);
+        const unsigneds_vec& c_vBuf = encoder.getBuf();
 
-    std::copy(c_vBuf.begin(), c_vBuf.end(),
-              std::ostream_iterator<unsigned>(std::cout, " "));
-    std::cout << std::endl;
-
-    CDecoder decoder(c_uParam);
-    const unsigned c_uDecoded = decoder.getBits(c_vBuf);
-    assert(c_uDecoded == c_uTestVal);
+        CDecoder decoder(c_uParam);
+        const unsigned c_uDecoded = decoder.getBits(c_vBuf);
+        assert(c_uDecoded == c_uTestVal);
+    }
 
     return 0;
 }
